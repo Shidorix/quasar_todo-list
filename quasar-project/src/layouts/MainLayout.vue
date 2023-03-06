5cd11bf283d9947b94b6dba783401c8c6c0cbb01
shidorix <danila.mazepa@gmail.com>
2023-03-06 23:32:25 +0300

    Реализовал Quasar. Поменял стилистику.

---
+++ quasar-project/src/layouts/MainLayout.vue
@@ -0,0 +1,126 @@
+<template>
+  <q-layout view="lHh lpR fFf">
+
+    <q-header bordered class="bg-grey-7 text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+      </q-toolbar>
+        
+        <div class="q-px-lg q-pt-xl q-mb-md items-center row">
+          <q-img src="../assets/logo1.png" style="width: 70px; height: 70px;" ></q-img>
+          <div class="text-h3 q-pl-md">
+            Shidorix's App
+          </div>
+          <q-img class="header-image absolute-top" src="../assets/header-img2.jpg"></q-img>
+        </div>  
+
+      <!-- <q-tabs align="left">
+        <q-route-tab to="/page1" label="Page One" />
+        <q-route-tab to="/page2" label="Page Two" />
+        <q-route-tab to="/page3" label="Page Three" />
+      </q-tabs> -->
+    </q-header>
+
+    <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="200"
+        :breakpoint="400"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section>
+                Входящие
+              </q-item-section>
+            </q-item>
+
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
+
+              <q-item-section>
+                Избранное
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="send" />
+              </q-item-section>
+
+              <q-item-section>
+                Отправить
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="drafts" />
+              </q-item-section>
+
+              <q-item-section>
+                Проекты
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-scroll-area>
+
+        <q-img class="absolute-top" src="../assets/fon.jpg" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="../assets/monk.jpg">
+            </q-avatar>
+            <div class="text-weight-bold">Danila Mazepa</div>
+            <div>@shidorix</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer bordered class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          <div>Ne pridumal</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+.header-image
+  height: 100%
+  z-index: -1
+  opacity: 0.2
+  // filter: grayscale(100%)
+</style>
