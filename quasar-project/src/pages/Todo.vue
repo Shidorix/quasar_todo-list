91859bf6038f9ed22b4510f57ea5ff8abdcf2f2e
shidorix <danila.mazepa@gmail.com>
2023-03-08 15:26:06 +0300

    Реализована база списка задач

--- quasar-project/src/pages/Todo.vue
+++ quasar-project/src/pages/Todo.vue
@@ -1,10 +1,45 @@
 <template>
-  <q-page class="flex flex-center">
-    <h2>ToDo</h2>
+  <q-page class="bg-grey-3 column">
+    
+    <q-list>
+
+      <q-item tag="label" v-ripple
+      v-for="task in tasks" :key="task.title">
+        <q-item-section avatar>
+          <q-checkbox v-model="color" val="teal" color="teal" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label> {{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      
+    </q-list>
   </q-page>
 </template>
 
 <script>
+
+export default {
+  data() {
+    return {
+      tasks: [
+        {
+          title: 'hello1',
+          done: false,
+        },
+        {
+          title: 'hello2',
+          done: false,
+        },
+        {
+          title: 'hello3',
+          done: false,
+        },
+      ]
+    }
+  }
+}
 
 </script>
 
